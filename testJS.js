f3f1f62f47fc6754df6eee4d719d8d9a9b4d0f3b
Gasan <saninazer@gmail.com>
2019-12-01 12:50:33 +0100

    communication between UI and back established. Still some costumization is needed.

--- testJS.js
+++ testJS.js
@@ -1,5 +1,27 @@
 "using strict"
 
-function myFunction(){
-    console.log("hello");
+let count = 0;
+
+function myFunction() {
+    let element = $("#elementSelector option:selected").text();
+    let startYear = $("#startYear option:selected").text();
+    let endYear = $("#endYear option:selected").text();
+    console.log(element);
+    console.log(startYear);
+    console.log(endYear);
+    let imageName = element + "_" + startYear + "_" + endYear + ".png";
+    console.log(imageName);
+
+    clear();
+    let imageParent = document.getElementById("body");
+    let image = document.createElement("img");
+    image.id = "id";
+    image.className = "classImg";
+    image.src = "images/charts/" + imageName; // image.src = "IMAGE URL/PATH"
+    imageParent.appendChild(image);
+
+}
+
+function clear() {
+    document.getElementById("body").innerHTML = "";
 }
