780dfb245b3d5a0295f89bd26efa47eb5209c046
Gasan <saninazer@gmail.com>
2019-12-06 18:38:32 +0100

    spark fixed

--- model/monthly_avg_spark.py
+++ model/monthly_avg_spark.py
@@ -1,6 +1,7 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import month
+import string
 
 import sys
 
@@ -11,24 +12,35 @@ sc = SparkContext(conf = conf)
 
 element = sys.argv[1]
 
-log_txt = sc.textFile("Madrid2001.csv")
+log_txt = sc.textFile("model/madrid_2001.csv")
 sqlContext = SQLContext(sc)
 header = log_txt.first()
 
 log_txt = log_txt.filter(lambda line: line != header)
-
 temp_var = log_txt.map(lambda k: k.split(","))
 log_df = temp_var.toDF(header.split(","))
 
 log_df2 = log_df.groupBy(month("Date").alias("Month")).agg({element: 'mean'}).orderBy("Month")
-# log_df2.write.format("csv").save("/home/ubuntu/output.csv")
+log_df2.show()
+log_df2.write.format("csv").save("model/output.csv")
+'''
+print(log_df2.collect())
+
+log_df2.show()
 
 
 time_period = "2001"
-time = log_df2["Month"]
-#ToDo: Fill in brackets below the name of the average column (i.e. "agg("CO")")
-#values = log_df2[]
+#time = log_df2.Month.collect()
+#ToDo: Fill in brackets below the name of the average column (i.e. 'avg(CO)')
+#values = log_df2[0]
+print("-------------")
+time = log_df2.select('Month').collect()
+values = log_df2.select('avg(CO)').collect()
+print("----------")
+
 unit = "unknown"
+#time = [12,13,14,15,16]
+#values = [1,2,3,4,5]
 
 # Plot the diagram based on processed data
 fig = plt.figure(dpi=128, figsize=(10, 6))
@@ -37,4 +49,5 @@ plt.title("Average " + element + " " + unit + " in Madrid (" + time_period + ")"
 plt.grid(True)
 plt.ylabel("Average " + element + " " + unit, fontsize=16)
 plt.xlabel("Year", fontsize=16)
-plt.savefig("/home/ubuntu/output.png")
+plt.savefig("model/output.png")
+'''
