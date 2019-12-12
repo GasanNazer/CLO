db750e37ac00f4b7e9f6a7afaacf143797f317f4
Gasan <saninazer@gmail.com>
2019-12-12 06:20:56 +0100

    commiting the model

--- model/combine/runner.py
+++ model/combine/runner.py
@@ -4,7 +4,7 @@ import os
 year = 2001
 elements = ["CO", "NO_2", "O_3", "PM10", "SO_2"]
 i = 0
-'''
+
 while year < 2018:
     while i < len(elements):
         myCmd = 'spark-submit monthly_avg_spark_fixed.py '  + elements[i] + " " + str(year)
@@ -36,7 +36,7 @@ while year < 2018:
         i = i + 1
     year = year + 1
     i = 0;
-'''
+
 i = 2001
 j = i + 1
 
